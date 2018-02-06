5eff4607de32be64a6d53a170e6bab36f3303d8f
Roxie Charchian <rmcharchian@gmail.com>
2018-02-06 06:10:28 -0600

    moving higher order components from App.js to withAuthentication.js  not done yet

--- src/components/App.js
+++ src/components/App.js
@@ -11,70 +11,92 @@ import SignInPage from './SignIn';
 import PasswordForgetPage from './PasswordForget';
 import HomePage from './Home';
 import AccountPage from './Account';
+import withAuthentication from './withAuthentication'; 
+// added when Route exact path was added.  Also changed export default
 
 import * as routes from '../constants/routes';
-import { firebase } from '../firebase';
+// import { firebase } from '../firebase';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      authUser: null,
-  };
-}
+//     this.state = {
+//       authUser: null,
+//   };
+// }
 
-componentDidMount() {
-  firebase.auth.onAuthStateChanged(authUser => {
-    authUser
-      ? this.setState(() => ({authUser}))
-      : this.setState(() => ({authUser: null}));
-  });
-}
+// componentDidMount() {
+//   firebase.auth.onAuthStateChanged(authUser => {
+//     authUser
+//       ? this.setState(() => ({authUser}))
+//       : this.setState(() => ({authUser: null}));
+//   });
+// }
+
 
 // const App = () =>
-render() {
-  return (
+// render() {
+//   return (
+//   <Router>
+//     <div>
+//       <Navigation authUser={this.state.authUser} />
+      
+//       <hr/>
+
+//       <Route
+//         exact path={routes.LANDING}
+//         component={() => <LandingPage />}
+//       />
+
+//       <Route
+//         exact path={routes.SIGN_UP}
+//         component={() => <SignUpPage />}
+//       />
+
+//       <Route
+//         exact path={routes.SIGN_IN}
+//         component={() => <SignInPage />}
+//       />
+
+//       <Route
+//         exact path={routes.PASSWORD_FORGET}
+//         component={() => <PasswordForgetPage />}
+//       />
+
+//       <Route
+//         exact path={routes.HOME}
+//         component={() => <HomePage/>}
+//       />
+
+//       <Route
+//         exact path={routes.ACCOUNT}
+//         component={() => <AccountPage/>}
+//       />
+
+//     </div>  
+//   </Router> 
+//   )
+// }  
+// } 
+
+// above being replaced by the following.  Should work the same.
+
+const App = () =>
   <Router>
     <div>
-      <Navigation authUser={this.state.authUser} />
-      
+      <Navigation />
+
       <hr/>
 
-      <Route
-        exact path={routes.LANDING}
-        component={() => <LandingPage />}
-      />
+      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+      <Route exact path={routes.HOME} component={() => <HomePage />} />
+      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+    </div>
+  </Router>
 
-      <Route
-        exact path={routes.SIGN_UP}
-        component={() => <SignUpPage />}
-      />
-
-      <Route
-        exact path={routes.SIGN_IN}
-        component={() => <SignInPage />}
-      />
-
-      <Route
-        exact path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
-
-      <Route
-        exact path={routes.HOME}
-        component={() => <HomePage/>}
-      />
-
-      <Route
-        exact path={routes.ACCOUNT}
-        component={() => <AccountPage/>}
-      />
-
-    </div>  
-  </Router> 
-  )
-}  
-} 
-
-export default App;
+// export default App;
+export default withAuthentication(App);
